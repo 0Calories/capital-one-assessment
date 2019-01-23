b592f02b03c3e79ecb0b1b360275359a159d6a2a
0Calories <cppanand@gmail.com>
2019-01-23 13:35:19 -0500

    Add testing files for parsing

---
+++ test.java
@@ -0,0 +1,20 @@
+/* This is a sample file that I am using to test the functionality of my line parser.
+ * Hopefully it works! 
+*/
+
+public class Test {
+	// Main method
+	public static void main(String[] args) {
+		int a = 0; // Trailing comment
+		int b = 1;
+		int c = 2;
+		/**/
+		String t = "Hi"; /* Trailing block comment */
+		int age = 22; /* Testing a trailing block comment that 
+		ends on 
+		another line
+		*/
+		// Here's a single line comment
+		// Just for safe measure
+	}
+}
---
+++ test2.js
@@ -0,0 +1,21 @@
+// Here's a second file to test the parsing on 
+/* Obligatory
+ * Block
+ * Comment
+*/
+
+const a = 1;
+const b = 2;
+const c = 3;
+
+const person = {
+	name: 'Ash',
+	age: 22,
+	hometown: 'Ottawa' // Born and raised
+};
+
+// Comment 
+/* Another
+block
+comment */
+
